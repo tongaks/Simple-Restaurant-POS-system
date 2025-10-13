d8f7c824586ba6a6297b9399582b47c0fdf51722
tongaks <rrfrokuu@gmail.com>
2025-10-13 11:37:23 +0800

    save point

--- Restaurant-POS-system/obj/Debug/net8.0-windows/Restaurant-POS-system.AssemblyInfo.vb
+++ Restaurant-POS-system/obj/Debug/net8.0-windows/Restaurant-POS-system.AssemblyInfo.vb
@@ -16,7 +16,7 @@ Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("Restaurant-POS-system"),  _
  Assembly: System.Reflection.AssemblyConfigurationAttribute("Debug"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
- Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3b2d113897d027b16af35666632af9b682fc299"),  _
+ Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3eecb338c2ccf0b9db88682a9dce805d3f9e8795"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("Restaurant-POS-system"),  _
  Assembly: System.Reflection.AssemblyTitleAttribute("Restaurant-POS-system"),  _
  Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0"),  _
